2731e094f66a983ab46cc4da4923576357e2ff21
geo_varenik <varenik_geo@mail.ru>
2020-08-29 15:54:43 +0300

    set up accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import CreateUser
+from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+class Signup(CreateView):
+    form_class = CreateUser
+    success_url = reverse_lazy('/')
+    template_name = 'accounts/signup.html'
+
+
+class UserPage(DetailView):
+    model = get_user_model()
+
--- shop/settings.py
+++ shop/settings.py
@@ -15,6 +15,8 @@ from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates/')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'goods',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static/'), ]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
